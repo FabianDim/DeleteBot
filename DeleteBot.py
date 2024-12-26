916a3dbbafd48dc1bcec0e4ada52a2854d9a9a5e
Fabian Di Mento <fdimento33@gmail.com>
2024-12-27 01:49:49 +1100

    Init Commit

---
+++ DeleteBot.py
@@ -0,0 +1,17 @@
+import discord
+import sys
+sys.path.append('../')
+from DiscordKey import key
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+
+client.run(key)
---
+++ README.md
@@ -0,0 +1,2 @@
+# Delete Bot
+ Python discord bot
