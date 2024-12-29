4b12e7f0506558b3949ea05978004ba00360b5fb
Fabian Di Mento <fdimento33@gmail.com>
2024-12-29 20:35:04 +1100

    Fixed issue with the key

--- DeleteBot.py
+++ DeleteBot.py
@@ -1,17 +1,28 @@
 import discord
 import sys
-sys.path.append('../')
-from DiscordKey import key
+import os
+from dotenv import load_dotenv
+from Logger import Logger
+
+# Explicitly specify the path to the .env file
+dotenv_path = 'C:\\Repos\\DeleteBot\\Key.env'
+load_dotenv(dotenv_path)
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+        if message.author == self.user:
+            return
 
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello World!')
+
+handler = Logger.handler
+key = os.getenv('DISCORD_KEY')
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-
-client.run(key)
+client.run(key, log_handler=handler)
