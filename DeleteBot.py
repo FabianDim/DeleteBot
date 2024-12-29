da4e5d8662dc6750e7ea0631709821d13ed97ce2
Fabian Di Mento <fdimento33@gmail.com>
2024-12-29 21:37:18 +1100

    Set up slash commands

--- DeleteBot.py
+++ DeleteBot.py
@@ -1,4 +1,7 @@
+from code import interact
 import discord
+from discord.ext import commands
+from discord import app_commands, guild
 import sys
 import os
 from dotenv import load_dotenv
@@ -8,21 +11,30 @@ from Logger import Logger
 dotenv_path = 'C:\\Repos\\DeleteBot\\Key.env'
 load_dotenv(dotenv_path)
 
-class MyClient(discord.Client):
+class MyClient(commands.Bot):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
+        try:
+            GUILD_ID = discord.Object(id=320425715310788618)
+            synced = await self.tree.sync(guild=GUILD_ID)
+            print(f'sync {len(synced)} commands to guild')
+        except Exception as e:
+            print(f'error syncing {e}')
     async def on_message(self, message):
         if message.author == self.user:
             return
 
-        if message.content.startswith('$hello'):
-            await message.channel.send('Hello World!')
-
 handler = Logger.handler
 key = os.getenv('DISCORD_KEY')
+
 intents = discord.Intents.default()
 intents.message_content = True
+client = MyClient(command_prefix="!", intents = intents)
+GUILD_ID = discord.Object(id=320425715310788618)
 
-client = MyClient(intents=intents)
+@client.tree.command(name="hello", description="Say Hello!", guild=GUILD_ID)
+async def sayHello(interaction: discord.Interaction):
+    await interaction.response.send_message("Hi there!")
+
 client.run(key, log_handler=handler)
